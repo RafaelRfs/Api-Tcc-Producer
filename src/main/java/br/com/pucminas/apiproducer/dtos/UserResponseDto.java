29bac0a71eca0a278eca737973365ed5196ef652
Rafael <leonsky0407@gmail.com>
2021-10-26 22:51:42 -0300

    ajustes v1

--- src/main/java/br/com/pucminas/apiproducer/dtos/UserResponseDto.java
+++ src/main/java/br/com/pucminas/apiproducer/dtos/UserResponseDto.java
@@ -1,6 +1,7 @@
 package br.com.pucminas.apiproducer.dtos;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.PropertyNamingStrategy;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
 import lombok.AllArgsConstructor;
@@ -15,8 +16,11 @@ import lombok.NoArgsConstructor;
 @JsonNaming(PropertyNamingStrategy.SnakeCaseStrategy.class)
 public class UserResponseDto {
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private Long usuarioId;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String nome;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
     private String token;
 
 }
